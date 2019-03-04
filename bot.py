881f481b4d19e8abd9bddb7653cf50ddab6daff8
Aleksandr Anisimov <eksyric5202@mail.ru>
2019-03-04 23:08:57 +0500

    Виртуальное окружение

--- bot.py
+++ bot.py
@@ -1,41 +1,53 @@
 # -*- coding: utf-8 -*-
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
+import settings
 # Updater подключается к Telegram, представляется ботом и проверяет входящие сообщения
 # CommandHandler - обработчик команд
 # MessageHandler - обработчик текстовых сообщений
-# Filters - с каким типом сообщений мы хотим взаимодействовать
-# logging - отчет о работе с ботом
+# Filters - с каким типом сообщений мы хотим взаимодействовать (текст, картинки и т.д.)
+# logging - отчет о работе с ботом 
 
 # Настроим прокси
-PROXY = {'proxy_url': 'socks5://t1.learn.python.ru:1080',
-    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
 
-logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
+
+logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO, # уровни логирования
                     handlers=[logging.FileHandler('bot.log', 'w', 'utf-8')]
                     )
+
 def greet_user(bot, update):
+    # bot -экземпляр нашего бота
+    # update - данные о прешедшем сообщении
     text ='Вызван /start'
-    print(text)
-    logging.info(text)
-    #  ответа пользователю
-    update.message.reply_text(text)
+    print(text) # Вывод текста в консоль
+    logging.info(text) # Сооб. в bot.log 
+    update.message.reply_text(text) # ответ пользователю
 
 def talk_to_me (bot, update):
-    user_text = update.message.text 
-    print(user_text)
-    # ответ пользователю
-    update.message.reply_text(user_text)
+    user_text ='Привет {}! Ты написал {}'.format(update.message.chat.first_name, update.message.text)
+    print(user_text) # печать вх. сообщен в консоль
+    # Сохраним в bot.log данные о вх. сообщ.
+    logging.info('Users: %s, Chat id: %s, Message: %s', update.message.chat.username,
+                update.message.chat.id, update.message.text)
+    update.message.reply_text(user_text) # ответ пользователю
  
 def main():
-    mybot = Updater("629476291:AAHapHDKGqjtE9Y4UbzHNDGFQo-X6dXD1KI", request_kwargs=PROXY)
+    # mybot - объект класса Updater
+    mybot = Updater(settings.API_KEY, request_kwargs=settings.PROXY)
     
     # Добавим в тело нашего бота обработчик команд
+    # dispatcher - перераспределяет вх. сообщения по "адресатам"
     dp = mybot.dispatcher
+   
+    # Если в telegram придет команда start мы на нее среагируем
+    # командой greet_user
     dp.add_handler(CommandHandler('start', greet_user))
     
     # Добавим в тело нашего бота обработчик сообщений
+    # Если поступило сообщение, то вызывается ф-ия talk_to_me
+    # Filters.text означает что MessageHandler будет реагировать только
+    # на текстовые сообщения
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
     
     logging.info('Бот запускается')
